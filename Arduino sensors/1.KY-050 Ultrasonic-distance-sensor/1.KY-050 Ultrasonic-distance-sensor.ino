9553a410e54d513bc01f3666cba594b1c77a9234
Stoyan <kilodox@abv.bg>
2018-03-17 18:28:11 +0200

    Adding codes for the Arduino Sensors.

---
+++ Arduino sensors/1.KY-050 Ultrasonic-distance-sensor/1.KY-050 Ultrasonic-distance-sensor.ino
@@ -0,0 +1,47 @@
+#define Echo_EingangsPin 7 // Echo input-pin
+#define Trigger_AusgangsPin 8 // Trigger output-pin
+ 
+int maximumRange = 300; 
+int minimumRange = 2; 
+long Abstand;
+long Dauer;
+ 
+void setup() {
+ pinMode(Trigger_AusgangsPin, OUTPUT);
+ pinMode(Echo_EingangsPin, INPUT);
+ Serial.begin (9600);
+}
+ 
+void loop() {
+ 
+ // Distance measurement will be started with a 10us long trigger signal
+ digitalWrite(Trigger_AusgangsPin, HIGH);
+ delayMicroseconds(10); 
+ digitalWrite(Trigger_AusgangsPin, LOW);
+  
+ // Now it will be waited at the echo input till the signal was activated
+ // and after that the time will be measured how long it is active 
+ Dauer = pulseIn(Echo_EingangsPin, HIGH);
+  
+ // Now the distance will be calculated with the recorded time
+ Abstand = Dauer/58.2;
+  
+ /* Check if the measured value is in the permitted range*/
+ if (Abstand >= maximumRange || Abstand <= minimumRange)
+ {
+    // An error message will be shown if it's not
+      Serial.println("Distance is not in the permitted range");
+      Serial.println("-----------------------------------");
+ }  
+  
+ else
+ {
+    // The calculated distance will be shown at the serial output
+      Serial.print("The distance is: ");
+      Serial.print(Abstand);
+      Serial.println("cm");
+      Serial.println("-----------------------------------");
+ }
+  // Break between single measurements
+ delay(500);
+}
---
+++ Arduino sensors/README.md
@@ -0,0 +1,20 @@
+# FDIBA-Car
+
+First task.
+
+Testing codes for:
+	
+	1.KY-050 Ultrasonic_Distance_Sensor
+![alt text](https://raw.githubusercontent.com/kilodox/FDIBA-Car/master/C:\Arduino projects\FDIBA-Car\Arduino sensors/KY-0050.png)
+	2.Sharp_IR_Sensor
+![alt text](https://raw.githubusercontent.com/kilodox/FDIBA-Car/master/C:\Arduino projects\FDIBA-Car\Arduino sensors/sharp-gp2d12-ir-range-sensor.jpg)
+-----------
+Second task.
+
+Testing codes for:
+
+	1.Devantech SRF10 Ultrasonic Range Finder
+![alt text](https://raw.githubusercontent.com/kilodox/FDIBA-Car/master/C:\Arduino projects\FDIBA-Car\Arduino sensors/Devantech SRF10 Ultrasonic Range Finder.jpg)
+	2.Sharp IR Distance Sensor GP2Y0A41SK0F (4-30CM)
+
+![alt text](https://raw.githubusercontent.com/kilodox/FDIBA-Car/master/C:\Arduino projects\FDIBA-Car\Arduino sensors/Sharp IR Distance Sensor GP2Y0A41SK0F (4-30CM).jpg)
