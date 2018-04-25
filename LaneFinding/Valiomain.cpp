9a07bf7d342ecd5742454f87c6c8ec2e9d7aa3f6
ValentinPantonov <valentin.antonov95@gmail.com>
2018-04-25 17:57:02 +0300

    Added a perspective warp into bird view

--- LaneFinding/Valiomain.cpp
+++ LaneFinding/Valiomain.cpp
@@ -5,8 +5,11 @@
 
 using namespace std;
 using namespace cv;
+using namespace cv::cuda;
 
-cv::Mat gray, frame, output, edges;
+cv::Mat imageROI, gray, frame, binary, canny, Perspective_Matrix(2, 4, CV_32FC1), Perspective_Matrix_inverse, Perspective;
+Point2f inputRect[4];
+Point2f outputRect[4];
 
 int main()
 {
@@ -15,22 +18,45 @@ int main()
 		return -1;
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-
+	
 	while (1)
 	{
 		cap >> frame; // get a new frame from camera
-		cv::cvtColor(frame, gray, CV_BGR2GRAY);
-		cv::threshold(gray, output, 150, 255, cv::THRESH_BINARY);
-
-		GaussianBlur(output, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
+				
+		Perspective_Matrix = Mat::zeros(frame.rows, frame.cols, frame.type());
+	
+		inputRect[0] = Point2f(0, 0);
+		inputRect[1] = Point2f(frame.cols, 0);
+		inputRect[2] = Point2f(frame.cols, frame.rows);
+		inputRect[3] = Point2f(0, frame.rows);
+														   
+		outputRect[0] = Point2f(0, 0);
+		outputRect[1] = Point2f(frame.cols, 0);
+		outputRect[2] = Point2f(frame.cols - 210, frame.rows);
+		outputRect[3] = Point2f(210, frame.rows);
 		
+		Perspective_Matrix = getPerspectiveTransform(inputRect, outputRect);
+		invert(Perspective_Matrix, Perspective_Matrix_inverse);
+
+		warpPerspective(frame, Perspective, Perspective_Matrix, frame.size());
+		imageROI = Perspective.clone();
+		Perspective = Perspective(Rect(0, 200, Perspective.cols, (Perspective.rows - 200)));
+
+		cv::cvtColor(Perspective, gray, CV_BGR2GRAY);
+		cv::threshold(gray, binary, 160, 255, cv::THRESH_BINARY);
+
+		GaussianBlur(binary, canny, Size(7, 7), 1.5, 1.5);
+		Canny(canny, canny, 0, 30, 3);
+
+
 		namedWindow("normal camera", WINDOW_AUTOSIZE);
 		namedWindow("edges", WINDOW_AUTOSIZE);
 		namedWindow("binary", WINDOW_AUTOSIZE);
+		namedWindow("perspective", WINDOW_AUTOSIZE);
 		imshow("normal camera", frame);
-		imshow("edges", edges);
-		imshow("binary", output);
+		imshow("edges", canny);
+		imshow("binary", binary);
+		imshow("perspective", imageROI);
 		if (waitKey(30) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
