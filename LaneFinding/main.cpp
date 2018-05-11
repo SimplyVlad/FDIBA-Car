41e980f434ada0bbc817a3ceec279245c56518fa
stoychos <stoychos01@gmail.com>
2018-05-11 22:43:26 +0300

    change main

--- LaneFinding/main.cpp
+++ LaneFinding/main.cpp
@@ -1,93 +1,64 @@
-#include <opencv2/core/core.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "Image.h"
-#include <iostream> 
-#include <vector>
-#include "polyfit.hpp"
+#include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <chrono>
 
+using namespace std;
 using namespace cv;
+using namespace cv::cuda;
+
+cv::Mat imageROI, gray, frame, binary, canny, Perspective_Matrix(2, 4, CV_32FC1), Perspective_Matrix_inverse, Perspective;
+Point2f inputRect[4];
+Point2f outputRect[4];
+
+int main()
+{
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	
+	while (1)
+	{
+		cap >> frame; // get a new frame from camera
+				
+		Perspective_Matrix = Mat::zeros(frame.rows, frame.cols, frame.type());
+	
+		inputRect[0] = Point2f(0, 0);
+		inputRect[1] = Point2f(frame.cols, 0);
+		inputRect[2] = Point2f(frame.cols, frame.rows);
+		inputRect[3] = Point2f(0, frame.rows);
+														   
+		outputRect[0] = Point2f(0, 0);
+		outputRect[1] = Point2f(frame.cols, 0);
+		outputRect[2] = Point2f(frame.cols - 210, frame.rows);
+		outputRect[3] = Point2f(210, frame.rows);
+		
+		Perspective_Matrix = getPerspectiveTransform(inputRect, outputRect);
+		invert(Perspective_Matrix, Perspective_Matrix_inverse);
+
+		warpPerspective(frame, Perspective, Perspective_Matrix, frame.size());
+		imageROI = Perspective.clone();
+		Perspective = Perspective(Rect(0, 200, Perspective.cols, (Perspective.rows - 200)));
+
+		cv::cvtColor(Perspective, gray, CV_BGR2GRAY);
+		cv::threshold(gray, binary, 160, 255, cv::THRESH_BINARY);
+
+		GaussianBlur(binary, canny, Size(7, 7), 1.5, 1.5);
+		Canny(canny, canny, 0, 30, 3);
 
 
-int findLeftLineCenter(std::vector<int>& v) {
-	int max = 0,x=0;
-	for (int i = 0;i < 320;i++) {
-		if (v[i] > max) {
-			max = v[i];
-			x = i;
-		}
+		namedWindow("normal camera", WINDOW_AUTOSIZE);
+		namedWindow("edges", WINDOW_AUTOSIZE);
+		namedWindow("binary", WINDOW_AUTOSIZE);
+		namedWindow("perspective", WINDOW_AUTOSIZE);
+		imshow("normal camera", frame);
+		imshow("edges", canny);
+		imshow("binary", binary);
+		imshow("perspective", imageROI);
+		if (waitKey(30) >= 0) break;
 	}
-	return x;
-}
-
-int findRightLineCenter(std::vector<int>& v) {
-	int max = 0, x = 320;
-	for (int i = 320;i < 640;i++) {
-		if (v[i] > max) {
-			max = v[i];
-			x = i;
-		}
-	}
-	return x;
-}
- 
-int main() {
-	 
-	 
-	Image src(imread("camera.jpg", IMREAD_GRAYSCALE));
-	Image dst;
-	 
-//	src.applyGaussianBlur(dst);
-	
-	
-
-	src.applyHistogramEqualizer(dst);
-	 
-	//src.applyThreshold(dst, 190, 255);
-	
- 
-	int bottom_leftx = 110;
-	int bottom_lefty = 480;
-
-	int upper_leftx = 260;
-	int upper_lefty = 120;
-
-	int upper_rightx = 410;
-	int upper_righty = 120;
-
-	int bottom_rightx = 560;
-	int bottom_righty = 480;
-
-
-	cv::Point2f src_vertices[4];
-	src_vertices[0] = Point(bottom_leftx, bottom_lefty);
-	src_vertices[1] = Point(upper_leftx, upper_lefty);
-	src_vertices[2] = Point(upper_rightx, upper_righty);
-	src_vertices[3] = Point(bottom_rightx, bottom_righty);
-	
-
-	Point2f dst_vertices[4];
-	dst_vertices[0] = Point(0, 480);
-	dst_vertices[1] = Point(0, 0);
-	dst_vertices[2] = Point(640, 0);
-	dst_vertices[3] = Point(640, 480);
-
- 
-	Image dst1;
-	 
-	dst.getBirdsView(src_vertices, dst_vertices, dst1);
-
-	dst1.applyThreshold(dst1, 190, 255);
-	dst1.show();
- 
-	 
-	waitKey();
+	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
 }
-
-
-
-
-
-
